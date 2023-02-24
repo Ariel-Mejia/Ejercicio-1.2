2e94667d3d58f812a7d8d73ea8141d01ad99fe82
Ariel-Mejia <arielmejia1996@yahoo.com>
2023-02-24 02:41:27 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2/Tarea2/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Tarea2.Models;
+using Tarea2.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace Tarea2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var informacion = new Models.Informacion
+            {
+                nombres = nombres.Text,
+                apellidos = apellidos.Text,
+                edad = edad.Text,
+                correo = correo.Text,
+
+            };
+
+            var pagina = new Views.PageTwo();
+            pagina.BindingContext = informacion;
+            await Navigation.PushAsync(pagina);
+
+        }
+    }
+}
+
+
+
---
+++ Tarea2/Tarea2/Models/Informacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2.Models
+{
+    public class Informacion
+    {
+        public string nombres { get; set; }
+
+        public string apellidos { get; set; }
+
+        public string edad { get; set; }
+
+        public string correo { get; set; }
+    }
+}
